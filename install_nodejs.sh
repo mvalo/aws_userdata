e050f4fe9d3b3e3c6fa721647c0ba862efebfce9
mvalo <maxime.valo@canal-plus.com>
2014-05-23 18:22:30 +0200

    Update install_nodejs.sh

--- install_nodejs.sh
+++ install_nodejs.sh
@@ -1,19 +1,19 @@
-// Mise à jour de la distrib.
+# Mise à jour de la distrib.
 yum -y update
-// Installation des outils de dev, compilateurs etc...
+# Installation des outils de dev, compilateurs etc...
 yum -y groupinstall "Development Tools"
 
-// Se placer dans le répertoire de travail
+# Se placer dans le répertoire de travail
 cd /usr/src
-// Récupération d'une version spécifique node, pour le moment c'est la 0.10.26 mais peut changer...
+# Récupération d'une version spécifique node, pour le moment c'est la 0.10.26 mais peut changer...
 wget http://nodejs.org/dist/v0.10.26/node-v0.10.26.tar.gz
-// Décompression
+# Décompression
 tar zxf node-v0.10.26.tar.gz
 cd node-v0.10.26
-// Configuration + compilation + installation globale sur l'OS
+# Configuration + compilation + installation globale sur l'OS
 ./configure
 make
 make install
 
-// Vérification de la version de node en cours
+# Vérification de la version de node en cours
 node -v
